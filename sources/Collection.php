65326acbde7067c4224a5a3ff7ccf9e72380d91c
Johnny Saymon <saymon@ciebit.com.br>
2018-07-27 23:57:49 -0300

    Criado models de Story, Status e Collection

---
+++ sources/Collection.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Ciebit\Stories;
+
+use ArrayIterator;
+use ArrayObject;
+use Countable;
+
+class Collection implements Countable
+{
+    private $stories; #:ArrayObject
+
+    public function __construct()
+    {
+        $this->stories = new ArrayObject;
+    }
+
+    public function add(Story $story): self
+    {
+        $this->stories->append($story);
+        return $this;
+    }
+
+    public function getIterator(): ArrayIterator
+    {
+        return $this->stories->getIterator();
+    }
+
+    public function count(): int
+    {
+        return $this->stories->count();
+    }
+}
