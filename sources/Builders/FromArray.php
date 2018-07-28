0cd32af97fff0a8c88916d9812e6c2d6f0abc565
Johnny Saymon <saymon@ciebit.com.br>
2018-07-27 23:58:53 -0300

    Criado builder para array

---
+++ sources/Builders/FromArray.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace Ciebit\Stories\Builders;
+
+use DateTime;
+use Exception;
+use Ciebit\Stories\Builders\Builder;
+use Ciebit\Stories\Status;
+use Ciebit\Stories\Story;
+
+class FromArray implements Builder
+{
+    private $data; #:array
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function build(): Story
+    {
+        if (! isset($this->data['title'])) {
+            throw new Exception('ciebit.stories.builders.invalid', 3);
+        }
+
+        $story = new Story($this->data['title'], Status::DRAFT());
+
+        isset($this->data['datetime'])
+        && $story->setDateTime(new DateTime($this->data['datetime']));
+
+        isset($this->data['id'])
+        && $story->setId((int) $this->data['id']);
+
+        isset($this->data['body'])
+        && $story->setStory((string) $this->data['body']);
+
+        isset($this->data['summary'])
+        && $story->setSummary((string) $this->data['summary']);
+
+        isset($this->data['uri'])
+        && $story->setUri((string) $this->data['uri']);
+
+        isset($this->data['views'])
+        && $story->setViews((int) $this->data['views']);
+
+        isset($this->data['status'])
+        && $story->setStatus(new Status((int) $this->data['status']));
+
+        return $story;
+    }
+}
