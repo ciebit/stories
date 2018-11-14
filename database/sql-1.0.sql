ae352a437471102f591419b23b5d2279da67c691
Maike <maike_jbe@hotmail.com>
2018-11-14 10:17:44 -0300

    Atualizar script sql

--- database/sql-1.0.sql
+++ database/sql-1.0.sql
@@ -1,12 +1,15 @@
-CREATE TABLE IF NOT EXISTS `cb_stories` (
-  `id`       int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `title`    varchar(300) DEFAULT NULL,
-  `summary`  varchar(500) DEFAULT NULL,
-  `body`     text,
+CREATE TABLE `cb_stories` (
+  `id` int(5) UNSIGNED NOT NULL NULL AUTO_INCREMENT,
+  `title` varchar(300) DEFAULT NULL,
+  `summary` varchar(500) DEFAULT NULL,
+  `body` text,
   `datetime` datetime DEFAULT NULL,
-  `uri`      varchar(300) DEFAULT NULL,
-  `views`    int(10) unsigned NOT NULL DEFAULT '0',
-  `language`    varchar(6) DEFAULT NULL,
-  `status`   tinyint(1) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='version:1.0';
+  `uri` varchar(300) DEFAULT NULL,
+  `views` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `language` varchar(6) DEFAULT NULL,
+  `languages_references` varchar(300) DEFAULT NULL,
+  `status` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='version:1.0';
+
+ALTER TABLE `cb_stories`
+  ADD PRIMARY KEY (`id`);
